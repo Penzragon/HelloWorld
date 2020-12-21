67e308ee16f02d9263c351203cb85c3f0a637e78
Penzragon <rifkyaliffa@gmail.com>
2020-12-21 21:27:06 +0700

    master

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,5 @@
 def hello(name):
-    return "Hello World! My name is {}".format(name)
+    return "Hello World! My name is {}.".format(name.title())
 
 x = input("Input your name: ")
 print(hello(x))
